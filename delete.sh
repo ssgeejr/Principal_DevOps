b34724c1b77cdc2a29992d6a690ba017dddbfe2e
Steve Gee <ioexcept@gmail.com>
2019-02-28 14:26:54 -0600

    created new file

---
+++ delete.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+registry='localhost:5000'
+name='demo/apline'
+version='3.7'
+echo "..............................."
+echo "REGISTRY: $registry"
+echo "IMAGE: $name"
+echo "VERSION: $version"
+echo "..............................."
+
+curl -v -sSL -X DELETE "http://${registry}/v2/${name}/manifests/$( \
+     curl -sSL -I \
+     -H "Accept: application/vnd.docker.distribution.manifest.v2+json" \
+     http://${registry}/v2/${name}/manifests/${version} \
+     | awk '$1 == "Docker-Content-Digest:" { print $2 }' \
+     | tr -d $'\r')"
